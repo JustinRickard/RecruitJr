a605887496280eb128c8f7433aaccb0c637601f8
Justin Rickard <justinr86.jr@gmail.com>
2017-09-23 16:46:11 +0100

    Update password helper

--- src/RecruitJr.Core/Helpers/PasswordHelper.cs
+++ src/RecruitJr.Core/Helpers/PasswordHelper.cs
@@ -4,9 +4,12 @@ namespace RecruitJr.Core.Helpers
 {
     public class PasswordHelper : IPasswordHelper
     {
+        int workFactor = 10;
+
         public string Encrypt(string password)
         {
-            return BCrypt.Net.BCrypt.HashPassword(password);
+            var salt = BCrypt.Net.BCrypt.GenerateSalt(workFactor);
+            return BCrypt.Net.BCrypt.HashPassword(password, salt);
         }
 
         public bool IsValid(string rawPassword, string hashedPassword) {
